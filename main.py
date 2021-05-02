cc4c325d118e3157d0b462fd511188c4b58b908b
John <46431837+jmmey@users.noreply.github.com>
2021-05-02 16:14:02 -0500

    InfluxDB data structure function working

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import re
 import time
+import json
 import requests
 from bs4 import BeautifulSoup as bs
 
@@ -45,6 +47,36 @@ def strip_table_row_tags(data):
     return channel_data
 
 
+def prep_influx_json(ds, us):
+    modem_data = []
+    DATA_TIME = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime())
+    # Downstream Data
+    for row in ds:
+        channel = row[0]
+        power = re.match(MEASURE_RE, row[5]).group(0)
+        snr = re.match(MEASURE_RE, row[6]).group(0)
+        ds_data = {
+            'measurement': 'modem_rf_stats',
+            'tags': {'direction': 'downstream'},
+            'time': DATA_TIME,
+            'fields': {'channel': channel, 'power': power, 'snr': snr}
+        }
+        modem_data.append(ds_data)
+    # Upstream Data
+    for row in us:
+        channel = row[0]
+        power = re.match(MEASURE_RE, row[-1]).group(0)
+        us_data = {
+            'measurement': 'modem_rf_stats',
+            'tags': {'direction': 'upstream'},
+            'time': DATA_TIME,
+            'fields': {'channel': channel, 'power': power}
+        }
+        modem_data.append(us_data)
+    json_body = json.dumps(modem_data)
+    return json_body
+
+
 def main():
     url = modem_url_request()
 
@@ -57,3 +89,4 @@ if __name__ == '__main__':
     us_rows = modem_us_table_rows(data_tables)
     ds_rows_clean = strip_table_row_tags(ds_rows)
     us_rows_clean = strip_table_row_tags(us_rows)
+    json_body = prep_influx_json(ds_rows_clean, us_rows_clean)
